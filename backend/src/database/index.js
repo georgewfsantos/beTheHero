3f36287aed90a145d4876dbf320ea3d8c0bb7655
George W F Santos <gweverton21@hotmail.com>
2020-03-27 16:44:32 -0300

    implement validation and tests

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,9 @@
 const knex = require("knex");
 const dbConfig = require("../../knexfile");
 
-const connection = knex(dbConfig.development);
+const config =
+  process.env.NODE_ENV === "test" ? dbConfig.test : dbConfig.development;
+
+const connection = knex(config);
 
 module.exports = connection;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
-//const connection = require("./database/index");
-
+const { celebrate, Segments, Joi } = require("celebrate");
 const OngController = require("./app/controllers/OngController");
 const IncidentController = require("./app/controllers/IncidentController");
 const IndividualOngController = require("./app/controllers/IndividualOngController");
@@ -13,18 +12,61 @@ routes.post("/sessions", SessionController.store);
 // ongs
 routes.get("/ongs", OngController.index);
 
-routes.post("/ongs", OngController.store);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .email()
+        .required(),
+      whatsapp: Joi.string()
+        .min(10)
+        .max(11)
+        .required(),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .length(2)
+        .required()
+    })
+  }),
+  OngController.store
+);
 
 // incidents
 
-routes.get("/incidents", IncidentController.index);
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.index
+);
 
 routes.post("/incidents", IncidentController.store);
 
-routes.delete("/incidents/:id", IncidentController.delete);
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentController.delete
+);
 
 // cases per ong
 
-routes.get("/ong_incidents", IndividualOngController.index);
+routes.get(
+  "/ong_incidents",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  IndividualOngController.index
+);
 
 module.exports = routes;
